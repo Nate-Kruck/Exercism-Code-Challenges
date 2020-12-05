e7a5a58584013e2fc39a3bfe41bce06723f3ac4a
Nathan Kruckenberg <natekruck1990@gmail.com>
2020-12-05 12:16:19 -0800

    test passing: empty str, a word, capatilized letter

---
+++ javascript/reverse-string/reverse-string.js
@@ -0,0 +1,11 @@
+
+export const reverseString = (str) => {
+  let newString = "";
+  for (let i = str.length -1; i >= 0; i--) {
+    newString += str[i];
+  }
+    return newString;
+}
+
+reverseString('robot');
+reverseString('Ramen');
