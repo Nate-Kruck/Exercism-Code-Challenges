64c015e4bb91507b7243a03ba8bd4b4eb733f099
Nathan Kruckenberg <natekruck1990@gmail.com>
2022-01-29 14:11:38 -0600

    download js hello world, testing to see if this pushes to github

--- javascript/hello-world/babel.config.js
+++ javascript/hello-world/babel.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   presets: [
     [
-      "@babel/preset-env",
+      '@babel/preset-env',
       {
         targets: {
-          node: "current",
+          node: 'current',
         },
-        useBuiltIns: "entry",
-        corejs: 3,
+        useBuiltIns: 'entry',
+        corejs: '3.14',
       },
     ],
   ],
-  plugins: ["@babel/plugin-syntax-bigint"],
+  plugins: ['@babel/plugin-syntax-bigint'],
 };
